dbd12505aa45034aca98bb50b8233fe0f62f567c
Chris Hanna <cdhannaphone@gmail.com>
2016-06-22 22:15:15 -0400

    cube

---
+++ 3d/3d/Game1.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Graphics.PackedVector;
+using Microsoft.Xna.Framework.Input;
+
+namespace _3d
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        
+        private Matrix viewMatrix, projectionMatrix, worldMatrix;
+        private List<VertexPositionColor> points;
+        private VertexBuffer vb;
+        private IndexBuffer ib;
+
+
+        private CubeVertexSet cube;
+        private BasicEffect fx; // TODO replace this with a custom hlsl file.
+
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+
+            // create the camera position
+            viewMatrix = Matrix.CreateLookAt(
+                new Vector3(0.0f, 0.0f, 3.0f),  // position
+                new Vector3(0, 0, 0),           // look at target
+                new Vector3(0, 1, 0)            // "up"
+                );
+
+            // create the matrix that transforms the world to the 2d screen. I stole this from the internet.
+            projectionMatrix = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45), GraphicsDevice.Viewport.AspectRatio, 0.01f, 100f);
+
+            // create a global world position
+            worldMatrix = Matrix.Identity;
+
+
+            // create a vertex buffer, and populate it with 4 positions. 
+            var vbData = new VertexPositionColor[]
+            {
+                new VertexPositionColor(new Vector3(0, 0, .5f), Color.Red), // (0,0) top left
+                new VertexPositionColor(new Vector3(1, 0, .5f), Color.White), // (1,0) top right
+                new VertexPositionColor(new Vector3(1, 1, 0), Color.Blue), // (1,1) bot right
+                new VertexPositionColor(new Vector3(0, 1, 0), Color.Yellow), // (0,1) bot left
+            };
+            vb = new VertexBuffer(GraphicsDevice, typeof(VertexPositionColor), vbData.Length, BufferUsage.WriteOnly); 
+            vb.SetData(vbData);
+
+            // create an index buffer, and populate it data to draw a triangle from (0,0) to (1,0) to (1,1), and a second from (0,0) to (1,1) to (0,1)
+            var ibData = new short[]
+            {
+                0,1,2, 0,2,3
+            };
+            ib = new IndexBuffer(GraphicsDevice, typeof(short), ibData.Length, BufferUsage.WriteOnly);
+            ib.SetData(ibData);
+
+
+            // create the basic effect used to interp the vertices on the graphics card. TODO, write our own. 
+            fx = new BasicEffect(GraphicsDevice);
+            fx.VertexColorEnabled = true;
+            fx.View = viewMatrix;
+            fx.World = worldMatrix;
+            fx.Projection = projectionMatrix;
+           
+            cube = new CubeVertexSet();
+            cube.Init(GraphicsDevice);
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            worldMatrix *= Matrix.CreateRotationY(.01f);
+            fx.World = worldMatrix;
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // set cull to none, otherwise we might not see shit. This is only for testing, in production, we'd be smart enough to know which way our polgons were facing.
+            //GraphicsDevice.RasterizerState = RasterizerState.CullNone;
+            
+            //GraphicsDevice.SetVertexBuffer(vb); // send the vertex buffer to the graphics card.
+            //GraphicsDevice.Indices = ib; // send the index buffer to the graphics card (maybe?)
+            //fx.LightingEnabled = true;
+
+            foreach (EffectPass pass in fx.CurrentTechnique.Passes)
+            {
+                pass.Apply(); // sends pass to gfx. 
+                
+                cube.Draw(GraphicsDevice);
+                // GraphicsDevice.DrawIndexedPrimitives(PrimitiveType.TriangleList, 0, 0, 4, 0, 2);      
+            }
+           
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ 3d/3d/VertexSet.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using OpenTK.Graphics;
+
+namespace _3d
+{
+    class Primitive
+    {
+
+        public VertexBuffer VertexBuffer { get; private set; }
+        public IndexBuffer IndexBuffer { get; private set; }
+
+        public void Init(GraphicsDevice gfx)
+        {
+            var pts = GetPoints();
+            VertexBuffer = new VertexBuffer(gfx, typeof(VertexPositionColor), pts.Length, BufferUsage.None);
+            VertexBuffer.SetData(pts);
+
+            var indicies = GetIndicies();
+            IndexBuffer = new IndexBuffer(gfx, typeof (short), indicies.Length, BufferUsage.None);
+            IndexBuffer.SetData(indicies);
+        }
+
+        protected virtual VertexPositionColor[] GetPoints()
+        {
+            // fill in points. 
+            return new VertexPositionColor[]{};
+        }
+
+        protected virtual short[] GetIndicies()
+        {
+            return new short[]{};
+        }
+
+        public void Draw(GraphicsDevice gfx)
+        {
+            gfx.SetVertexBuffer(VertexBuffer);
+            gfx.Indices = IndexBuffer;
+            gfx.RasterizerState = RasterizerState.CullNone;
+            gfx.DrawIndexedPrimitives(PrimitiveType.TriangleList, 0, 0, VertexBuffer.VertexCount, 0, IndexBuffer.IndexCount / 3);
+
+        }
+
+    }
+
+    class CubeVertexSet : Primitive
+    {
+
+
+        protected override VertexPositionColor[] GetPoints()
+        {
+            return new VertexPositionColor[]
+            {
+                new VertexPositionColor(new Vector3(0, 0, 0), Color.Red), 
+                new VertexPositionColor(new Vector3(1, 0, 0), Color.Red), 
+                new VertexPositionColor(new Vector3(1, 1, 0), Color.Green), 
+                new VertexPositionColor(new Vector3(0, 1, 0), Color.Green), 
+                new VertexPositionColor(new Vector3(0, 0, 1), Color.Blue), 
+                new VertexPositionColor(new Vector3(1, 0, 1), Color.Blue), 
+                new VertexPositionColor(new Vector3(1, 1, 1), Color.White), 
+                new VertexPositionColor(new Vector3(0, 1, 1), Color.White), 
+            }.ToList().Select(v => new VertexPositionColor(v.Position -= new Vector3(.5f), v.Color)).ToArray();
+        }
+
+        protected override short[] GetIndicies()
+        {
+            return new short[]
+            {
+                0, 1, 2,
+                0, 2, 3,
+
+                0, 1, 5,
+                0, 5, 4,
+
+                0, 4, 7,
+                0, 7, 3,
+
+                1, 5, 6,
+                1, 6, 2,
+
+                5, 6, 4,
+                4, 6, 7,
+
+                2, 6, 7,
+                2, 3, 7,
+            };
+        }
+    }
+}
